ae9bdde800a8f96345af383e7f8b7b43305ca132
Ana <puscasu.a.m@gmail.com>
2020-07-03 16:33:08 +0300

    Add - added AutoMapper

---
+++ API/Dtos/ProductToReturnDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Dtos
+{
+    public class ProductToReturnDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public decimal NetWeigh { get; set; }
+
+        public int Valability { get; set; }
+
+        public string PictureUrl { get; set; }
+
+        public string Description { get; set; }
+
+        public string ProductType { get; set; }
+
+        public string DietType { get; set; }
+
+    }
+}
