e108e74dc42a6451b69d43de73c0342c96c41bd4
Ana <puscasu.a.m@gmail.com>
2020-07-03 15:20:55 +0300

    Add - added generic repo

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Core.Entities;
 using Core.Interfaces;
+using Core.Specifications;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -13,17 +14,24 @@ namespace API.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
-        private readonly IProductRepository _repo;
+        private readonly IGenericRepository<Product> _productRepo;
+        private readonly IGenericRepository<DietType> _productDietRepo;
+        private readonly IGenericRepository<ProductType> _productTypeRepo;
 
-        public ProductsController(IProductRepository repo) 
+        public ProductsController(IGenericRepository<Product> productRepo, IGenericRepository<DietType> productDietRepo, 
+            IGenericRepository<ProductType> productTypeRepo) 
         {
-            _repo = repo;
+            _productRepo = productRepo;
+            _productDietRepo = productDietRepo;
+            _productTypeRepo = productTypeRepo;
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts() 
         {
-            var products = await _repo.GetProductsAsync();
+            var spec = new ProductsWithTypesAndDietsTypesSpecification();
+
+            var products = await _productRepo.ListAsync(spec);
 
             return Ok(products);
         }
@@ -32,20 +40,21 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _repo.GetProductByIdAsync(id);
+            var spec = new ProductsWithTypesAndDietsTypesSpecification(id);
+
+            return await _productRepo.GetEntityWithSpec(spec);
         }
 
         [HttpGet("diets")]
         public async Task<ActionResult<IReadOnlyList<DietType>>> GetDietTypes()
         {
-            return Ok(await _repo.GetDietTypeAsync());
+            return Ok(await _productDietRepo.ListAllAsync());
         }
 
         [HttpGet("types")]
         public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
         {
-            return Ok(await _repo.GetProductTypeAsync());
+            return Ok(await _productTypeRepo.ListAllAsync());
         }
-
     }
 }
---
+++ Core/Specifications/ProductsWithTypesAndDietsTypesSpecification.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Core.Specifications
+{
+    public class ProductsWithTypesAndDietsTypesSpecification : BaseSpecification<Product>
+    {
+        public ProductsWithTypesAndDietsTypesSpecification(int id) : base(x => x.Id == id)
+        {
+            AddInclude(x => x.DietType);
+            AddInclude(x => x.ProductType);
+        }
+
+        public ProductsWithTypesAndDietsTypesSpecification()
+        {
+            AddInclude(x => x.DietType);
+            AddInclude(x => x.ProductType);
+        }
+    }
+}
