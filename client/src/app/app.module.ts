9d47845d65d71866170f5a3a4e6daa0cbab3e067
Ana <puscasu.a.m@gmail.com>
2020-07-05 15:20:05 +0300

    Fix - organising Angular files

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AngularMaterialModule } from './shared/angular-material-module'
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { CoreModule } from './core/core.module';
+import { AngularMaterialModule } from './shared/angular-material-module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     AngularMaterialModule,
+    HttpClientModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/shared/models/product.ts
@@ -0,0 +1,11 @@
+export interface IProduct {
+  id: number;
+  name: string;
+  price: number;
+  netWeigh: number;
+  valability: number;
+  pictureUrl: string;
+  description: string;
+  productType: string;
+  dietType: string;
+}
