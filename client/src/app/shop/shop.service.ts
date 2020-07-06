7192350e4727805ed515ec688b5c947431b33bcf
Ana <puscasu.a.m@gmail.com>
2020-07-06 17:00:08 +0300

    Add - added sort functionality - angular project

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -14,7 +14,7 @@ export class ShopService {
 
   constructor(private http: HttpClient) { }
 
-  getProducts(dietId?: number, typeId?: number) {
+  getProducts(dietId?: number, typeId?: number, sort?: string) {
 
     let params = new HttpParams;
 
@@ -24,6 +24,10 @@ export class ShopService {
 
     if (typeId) {
       params = params.append('typeId', typeId.toString());
+    }
+
+    if (sort) {
+      params = params.append('sort', sort);
     }
 
     return this.http.get<IPagination>(this.baseUrl + 'products', { observe: 'response', params })
