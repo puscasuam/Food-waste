6cd7b1c0cb3967d8775207e75312e30a09dafd5b
Ana <puscasu.a.m@gmail.com>
2020-07-07 15:46:04 +0300

    Add - added user database  - API project

---
+++ Infrastructure/Identity/AppIdentityDbContextSeed.cs
@@ -0,0 +1,38 @@
+﻿using Core.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Identity
+{
+    public class AppIdentityDbContextSeed
+    {
+        public static async Task SeedUsersAsync(UserManager<AppUser> userManager)
+        {
+            if (!userManager.Users.Any())
+            {
+                var user = new AppUser
+                {
+                    DisplayName = "Carmen",
+                    Email = "carmen@test.com",
+                    UserName = "carmen@test.com",
+                    Address = new Address
+                    {
+                        FirstName = "Carmen",
+                        LastName = "Pop",
+                        Street = "Bucuresti",
+                        City = "Cluj",
+                        State = "CJ",
+                    }
+                };
+
+                await userManager.CreateAsync(user, "Pa$$w0rd");
+            }
+        }
+
+
+    }
+}
